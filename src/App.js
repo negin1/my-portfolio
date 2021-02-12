115933363eb0056fa591164e799f2b124e31585d
negin1 <negin.jalalian@gmail.com>
2021-02-12 11:03:07 +0100

    removed the weather component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './components/pages/Home';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Weather from './components/Weather/Weather';
+
 
 
 function App() {
@@ -19,7 +19,6 @@ function App() {
         <Route path='/projects' component={Projects} />
         <Route path='/skills' component={Skills}/>
       </Switch>
-         <Weather />
     </Router>
     </>
   );
